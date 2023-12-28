1658bb80e26e89692765448352d59b32cc925427
hogelog <konbu.komuro@gmail.com>
2023-12-28 19:05:41 +0900

    Refactor

--- lib/metrics_monitor/monitor/unicorn.rb
+++ lib/metrics_monitor/monitor/unicorn.rb
@@ -1,18 +1,17 @@
 module MetricsMonitor
   class Monitor
     class Unicorn
-    end
-
-    begin
-      if defined?(::Unicorn)
-        def Unicorn.worker_processes
-          config = nil
-          ObjectSpace.each_object(::Unicorn::Configurator) {|c| config = c }
-          config.set[:worker_processes]
-        end
-      else
-        def Unicorn.worker_processes
-          nil
+      class << self
+        if defined?(::Unicorn)
+          def worker_processes
+            config = nil
+            ObjectSpace.each_object(::Unicorn::Configurator) {|c| config = c }
+            config.set[:worker_processes]
+          end
+        else
+          def worker_processes
+            nil
+          end
         end
       end
     end
